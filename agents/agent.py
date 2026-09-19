5888c083f12de93d750529835133bf2058e954f7
Malva Omar <malva.omar@stud.th-luebeck.de>
2026-09-19 18:20:23 +0200

    Agent Logic with session persistence  and ask_model() function

--- agents/agent.py
+++ agents/agent.py
@@ -1,1 +1,21 @@
-print("Hello Molts")
+import ollama
+
+
+messages=[
+    {"role": "system", "content": "You are a red team offensive pentester focused on identifying and exploiting vulnerabilities in target systems set by the user."}
+]
+
+def ask_model(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = ollama.chat(model="qwen3:8b",
+                           messages=messages,
+                           think=False,
+                           )
+    assistant_message = response.message.content
+    messages.append({"role": "assistant", "content": assistant_message})
+    return assistant_message
+
+while True:
+    user_input = input("You: ")
+    answer = ask_model(user_input)
+    print("Agent:", answer)
